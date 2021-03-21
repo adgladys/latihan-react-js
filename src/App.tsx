f9f78a24e3ccc2d25d04277ec9f39d3b8b46e758
Gladys Devina <gladys.devina@gmail.com>
2021-03-21 20:42:03 +0700

    week 8

--- src/App.tsx
+++ src/App.tsx
@@ -2,87 +2,136 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-interface IData {
-  id: string;
-  name: string;
-  email: string;
-}
-
+//week 8
 interface IAppState {
-  showTable: boolean;
+  fullname: string;
+  age: string;
 }
 
-interface IAppProps {
-  data: IData[]
-}
-
-class App extends React.Component<IAppProps, IAppState>{
+class App extends React.Component<{}, IAppState> {
   constructor(props) {
     super(props);
     this.state = {
-      showTable: true,
+      fullname: '',
+      age: '',
     }
   }
 
-  componentDidMount() {
-    alert("did mount App component !")
+  handleBtnClick = () => {
+    alert(`${this.state.fullname} ${this.state.age}`)
   }
 
-  componentWillUnmount() {
-    alert("unmounting App component !")
-  }
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {name} = event.target;
+    const {value} = event.target;
 
-  componentDidUpdate(){
-    alert("did update called !")
-  }
+    const newState = {...this.state};
+    newState[name] = value;
 
-  public toggleShowTable = () => {
-    const {showTable} = this.state;
-    this.setState({showTable: !showTable});
-  };
-
-  public renderRow = () => {
-    return this.props.data.map((item) => {
-      return (
-        <tr>
-          <td>{item.id}</td>
-          <td>{item.name}</td>
-          <td>{item.email}</td>
-        </tr>
-      )
-    })
-  }
-
-  public renderTable = () => {
-    if (this.state.showTable) {
-      return (
-        <table className = "table">
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>name</td>
-              <td>email</td>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderRow()}
-          </tbody>
-        </table>
-      )
-    }
+    this.setState(newState);
   }
 
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {this.renderTable()}
-          <button onClick={this.toggleShowTable}>test</button>
+      <div className = "App">
+        <header className = "App-header">
+          <img src={logo} className = "App-logo" alt= "logo" />
+          <form>
+            <input name= "fullname" value={this.state.fullname} placeholder="type fullname here" onChange={this.handleChange} />
+            <br/>
+            <input name="age" value={this.state.age} placeholder="type age here" onChange={this.handleChange} />
+            <br/>
+            <button type="button" onClick={this.handleBtnClick}>SAVE</button>
+          </form>
         </header>
       </div>
-    );
-  };
-};
+    )
+  }
+}
+
+//week 7
+
+// interface IData {
+//   id: string;
+//   name: string;
+//   email: string;
+// }
+
+// interface IAppState {
+//   showTable: boolean;
+// }
+
+// interface IAppProps {
+//   data: IData[]
+// }
+
+// class App extends React.Component<IAppProps, IAppState>{
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showTable: true,
+//     }
+//   }
+
+//   componentDidMount() {
+//     alert("did mount App component !")
+//   }
+
+//   componentWillUnmount() {
+//     alert("unmounting App component !")
+//   }
+
+//   componentDidUpdate(){
+//     alert("did update called !")
+//   }
+
+//   public toggleShowTable = () => {
+//     const {showTable} = this.state;
+//     this.setState({showTable: !showTable});
+//   };
+
+//   public renderRow = () => {
+//     return this.props.data.map((item) => {
+//       return (
+//         <tr>
+//           <td>{item.id}</td>
+//           <td>{item.name}</td>
+//           <td>{item.email}</td>
+//         </tr>
+//       )
+//     })
+//   }
+
+//   public renderTable = () => {
+//     if (this.state.showTable) {
+//       return (
+//         <table className = "table">
+//           <thead>
+//             <tr>
+//               <td>id</td>
+//               <td>name</td>
+//               <td>email</td>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {this.renderRow()}
+//           </tbody>
+//         </table>
+//       )
+//     }
+//   }
+
+//   public render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           {this.renderTable()}
+//           <button onClick={this.toggleShowTable}>test</button>
+//         </header>
+//       </div>
+//     );
+//   };
+// };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,25 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const dataDummy = [
-  {
-    id: "1",
-    name: "John",
-    email: "john@gmail.com"
-  },
-  {
-    id: "1",
-    name: "Frau",
-    email: "frau@gmail.com"
-  },
-]
+// week 8
 
 ReactDOM.render(
   <React.StrictMode>
-    <App data={dataDummy} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// week 7
+
+// const dataDummy = [
+//   {
+//     id: "1",
+//     name: "John",
+//     email: "john@gmail.com"
+//   },
+//   {
+//     id: "1",
+//     name: "Frau",
+//     email: "frau@gmail.com"
+//   },
+// ]
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App data={dataDummy} />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
